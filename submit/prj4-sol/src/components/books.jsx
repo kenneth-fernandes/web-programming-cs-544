4d580fcbef0fb4be635d1abb2e5626bd323956c6
kenneth-fernandes <kferna11@binghamton.edu>
2020-07-31 05:46:14 -0400

    Kenneth: Added the searchBox element with onBlur events

--- submit/prj4-sol/src/components/books.jsx
+++ submit/prj4-sol/src/components/books.jsx
@@ -6,19 +6,36 @@ export default class Books extends React.Component {
 
   constructor(props) {
     super(props);
+    //console.log(props,'',this);
     //@TODO other initialization
+    this.onBlur = this.onBlur.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   //@TODO other methods
+  onBlur(event) {
+    
+  }
+
+  onSubmit(event) {
+    
+  }
 
   render() {
     //@TODO complete rendering
-    return '';
+    const element = (
+      <form className="search">
+        <label htmlFor="search">
+          Search Catalog
+        </label>
+        <input name="authorsTitleSearch" id="search" onBlur={this.onBlur} />
+      </form>);
+    return element;
   }
 
 }
 
-export function Book({book, full}) {
+export function Book({ book, full }) {
   //@TODO return rendering of book based on full
   return '';
 }
