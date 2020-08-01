d44bef6c510f819891c15a40e2aa7fbf6b92fc34
kenneth-fernandes <kferna11@binghamton.edu>
2020-08-01 14:55:19 -0400

    Kenneth: Added implementation for cart.jsx

--- submit/prj4-sol/src/components/cart.jsx
+++ submit/prj4-sol/src/components/cart.jsx
@@ -13,7 +13,11 @@ export default class Cart extends React.Component {
 
   render() {
     //@TODO render cart by mapping LineItem over each item in props.items.
-    return '';
+    const { app, items } = this.props;
+    const lineItemComponentArr = items.map(function (item, index) {
+      return <LineItem app={app} item={item} key={index} />;
+    });
+    return lineItemComponentArr;
   }
 }
 
@@ -22,8 +26,34 @@ class LineItem extends React.Component {
   constructor(props) {
     super(props);
     //@TODO
+    this.state = { nUnits: props.item.nUnits };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  async onChange(event){
+    const nUnits = event.target.value;
+    this.setState({nUnits: nUnits});
+    await this.props.app.update(this.props.item.book, nUnits);
   }
 
   //@TODO other methods
+  render() {
 
+    const { book, nUnits } = this.props.item;
+    const option = [<option value="0" key="0">0 (Delete)</option>].concat(
+      Array.from({ length: 9 },
+        function (v, i) {
+          return <option key={i + 1} value={i + 1}>{i + 1}</option>;
+        }));
+    const select = <select value={nUnits} onChange={this.onChange}>
+      {option}
+    </select>;
+    return (<div className="line-item">
+      <Book book={book} full={false} />
+      <div className="nUnits">
+        <label>Quantity</label>
+        {select}
+      </div>
+    </div>);
+  }
 }
